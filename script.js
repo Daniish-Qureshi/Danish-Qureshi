c53d327b03d99d56a5e5ac93fa4316d18ac94b51
danish-qureshi <mrdanish7806@gmail.com>
2025-05-25 18:46:12 +0530

    update

--- script.js
+++ script.js
@@ -2,12 +2,8 @@
 
 
 
-/**
- * PRELOADER
- */
-
+/*PRELOADER*/
 const preloader = document.querySelector("[data-preloader]");
-
 window.addEventListener("DOMContentLoaded", function () {
   preloader.classList.add("loaded");
   document.body.classList.add("loaded");
@@ -15,10 +11,7 @@ window.addEventListener("DOMContentLoaded", function () {
 
 
 
-/**
- * add event on multiple elements
- */
-
+/*add event on multiple elements*/
 const addEventOnElements = function (elements, eventType, callback) {
   for (let i = 0, len = elements.length; i < len; i++) {
     elements[i].addEventListener(eventType, callback);
@@ -26,22 +19,16 @@ const addEventOnElements = function (elements, eventType, callback) {
 }
 
 
-
-/**
- * Mobile navbar toggle
- */
-
+/*Mobile navbar toggle*/
 const navbar = document.querySelector("[data-navbar]");
 const navTogglers = document.querySelectorAll("[data-nav-toggler]");
 const navLinks = document.querySelectorAll("[data-nav-link]");
 const overlay = document.querySelector("[data-overlay]");
-
 addEventOnElements(navTogglers, "click", function () {
   navbar.classList.toggle("active");
   overlay.classList.toggle("active");
   document.body.classList.toggle("nav-active");
 });
-
 addEventOnElements(navLinks, "click", function () {
   navbar.classList.remove("active");
   overlay.classList.remove("active");
@@ -49,88 +36,53 @@ addEventOnElements(navLinks, "click", function () {
 });
 
 
-
-/**
- * Header active
- */
-
+/*Header active*/
 const header = document.querySelector("[data-header]");
-
 window.addEventListener("scroll", function () {
   header.classList[window.scrollY > 100 ? "add" : "remove"]("active");
 });
 
 
-
-/**
- * Element tilt effect
- */
-
+/*Element tilt effect*/
 const tiltElements = document.querySelectorAll("[data-tilt]");
-
 const initTilt = function (event) {
 
   /** get tilt element center position */
   const centerX = this.offsetWidth / 2;
   const centerY = this.offsetHeight / 2;
-
   const tiltPosY = ((event.offsetX - centerX) / centerX) * 10;
   const tiltPosX = ((event.offsetY - centerY) / centerY) * 10;
-
   this.style.transform = `perspective(1000px) rotateX(${tiltPosX}deg) rotateY(${tiltPosY - (tiltPosY * 2)}deg)`;
-
 }
-
 addEventOnElements(tiltElements, "mousemove", initTilt);
-
 addEventOnElements(tiltElements, "mouseout", function () {
   this.style.transform = `perspective(1000px) rotateX(0deg) rotateY(0deg)`;
 });
 
 
-
-/**
- * Tab content
- */
-
+/*Tab content*/
 const tabBtns = document.querySelectorAll("[data-tab-btn]");
 const tabContents = document.querySelectorAll("[data-tab-content]");
-
 let lastActiveTabBtn = tabBtns[0];
 let lastActiveTabContent = tabContents[0];
-
 const filterContent = function () {
-
   if (!(lastActiveTabBtn === this)) {
-
     lastActiveTabBtn.classList.remove("active");
     lastActiveTabContent.classList.remove("active");
-
     this.classList.add("active");
     lastActiveTabBtn = this;
-
     const currentTabContent = document.querySelector(`[data-tab-content="${this.dataset.tabBtn}"]`);
-
     currentTabContent.classList.add("active");
     lastActiveTabContent = currentTabContent;
-
   }
-
 }
-
 addEventOnElements(tabBtns, "click", filterContent);
 
 
-
-/**
- * Custom cursor
- */
-
+/*Custom cursor*/
 const cursors = document.querySelectorAll("[data-cursor]");
 const hoveredElements = [...document.querySelectorAll("button"), ...document.querySelectorAll("a")];
-
 window.addEventListener("mousemove", function (event) {
-
   const posX = event.clientX;
   const posY = event.clientY;
 
@@ -143,7 +95,6 @@ window.addEventListener("mousemove", function (event) {
     cursors[1].style.left = `${posX}px`;
     cursors[1].style.top = `${posY}px`;
   }, 80);
-
 });
 
 /** add hovered class when mouseover on hoverElements */
@@ -152,6 +103,7 @@ addEventOnElements(hoveredElements, "mouseover", function () {
     cursors[i].classList.add("hovered");
   }
 });
+
 
 /** remove hovered class when mouseout on hoverElements */
 addEventOnElements(hoveredElements, "mouseout", function () {
